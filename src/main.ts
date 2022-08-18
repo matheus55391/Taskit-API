5065e834a94288a1fb7597c01bdd3338533526be
matheus55391 <matheus.manulo55@gmail.com>
2022-08-18 18:38:46 -0300

    base do projeto

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { INestApplication } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+
+async function bootstrap() {
+  const port = 3025;
+
+  //swagger
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('bkp-crud-api')
+    .setDescription('Uma api para controle dos planos de backup da Altatech')
+    .setVersion('0.1')
+    .addTag('backup')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //prisma
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(port);
+  console.log('http://localhost:' + port);
+}
+bootstrap();
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Injectable()
+export class TasksService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createTaskDto: CreateTaskDto) {
+    return await this.prisma.task.create({
+      data: {
+        title: createTaskDto.title,
+        description: createTaskDto.description,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.task.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
