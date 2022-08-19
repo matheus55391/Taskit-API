af368ac79354fffa466f184d4f44a6322e99711d
matheus55391 <matheus.manulo55@gmail.com>
2022-08-18 23:00:38 -0300

    configurando heroku

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,43 +18,43 @@ import { Console } from 'console';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    if (
-      !createUserDto.username ||
-      !createUserDto.password ||
-      createUserDto.username.length < 6 ||
-      createUserDto.password.length < 6
-    ) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          error: 'Invalid username or password',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    await this.usersService.create(createUserDto);
-    return;
-  }
+  // @Post()
+  // async create(@Body() createUserDto: CreateUserDto) {
+  //   if (
+  //     !createUserDto.username ||
+  //     !createUserDto.password ||
+  //     createUserDto.username.length < 6 ||
+  //     createUserDto.password.length < 6
+  //   ) {
+  //     throw new HttpException(
+  //       {
+  //         status: HttpStatus.BAD_REQUEST,
+  //         error: 'Invalid username or password',
+  //       },
+  //       HttpStatus.BAD_REQUEST,
+  //     );
+  //   }
+  //   await this.usersService.create(createUserDto);
+  //   return;
+  // }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
 }
